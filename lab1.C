e957b38225259b3d877fed59dc9b326c0cca21a2
mixailkornilin <mixail.kornilin@gmail.com>
2025-10-18 10:11:08 +0300

    Update lab1.C

--- lab1.C
+++ lab1.C
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 int main() {
-    printf("hellow");
+    double a, b, c, P;
+
+    printf("Введите катет a: ");
+    scanf("%lf", &a);
+    
+    printf("Введите катет b: ");
+    scanf("%lf", &b);
+    
+    c = sqrt(a*a+b*b);
+    P = a + b+ c;
+    
+    printf("Гипотенуза C = %.2f\n", c);
+    printf("Периметр P = %.2f\n", P);
+    
+    return 0;
 }
