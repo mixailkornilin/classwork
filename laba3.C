c16ce3816c5e9da35cac5de940a1a15f136b5a26
mixailkornilin <mixail.kornilin@gmail.com>
2026-01-23 22:09:01 +0300

    Update laba3.C

--- laba3.C
+++ laba3.C
@@ -9,7 +9,7 @@ int main() {
     int result = (A > 0) && (B > 0) && (C > 0);
     
     printf("\nЧисла: A = %d, B = %d, C = %d\n", A, B, C);
-    printf("Все числа положительные: %s\n", result ? "ИСТИНА" : "ЛОЖЬ");
+    printf("Все числа положительные?: %s\n", result ? "ИСТИНА" : "ЛОЖЬ");
     
     return 0;
 }
